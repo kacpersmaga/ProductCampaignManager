d1e649684c26f63f92b1ea3b0f20665a2f01d8a2
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-05-27 23:08:49 +0200

    fix(infrastructure): register ICampaignRepository in DI container

--- backend/ProductCampaignManager.API/Program.cs
+++ backend/ProductCampaignManager.API/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using ProductCampaignManager.Application.Interfaces;
 using ProductCampaignManager.Infrastructure.Persistence;
+using ProductCampaignManager.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<ICampaignRepository, CampaignRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
